a6a48255eee54c50ff3ecc484dc48a0756a52675
KitRifty <kitrifty@gmail.com>
2021-09-18 20:40:10 -0700

    Update Linux and Windows binaries Update buildscript to utilize custom hl2sdk-tf2 branch Fix compilation error with clang Resolve *__finite math symbols with linux builds

---
+++ extension/linux/glibc_compat.cpp
@@ -0,0 +1,29 @@
+#if __GNUC__ > 2 || \
+	(__GNUC__ == 2 && (__GNUC_MINOR__ > 3 || \
+		(__GNUC_MINOR__ == 3 && __GNUC_PATCHLEVEL__ > 0)))
+
+#include <math.h>
+
+extern "C" {
+	double __exp_finite(double x) { return exp(x); }
+	double __exp2_finite(double x) { return exp2(x); }
+	double __acos_finite(double x) { return acos(x); }
+	double __asin_finite(double x) { return asin(x); }
+	double __atan2_finite(double x, double y) { return atan2(x, y); }
+	double __log2_finite(double x) { return log2(x); }
+	double __log10_finite(double x) { return log10(x); }
+	double __log_finite(double x) { return log(x); }
+	double __pow_finite(double x, double y) { return pow(x, y); }
+
+	float __expf_finite(float x) { return expf(x); }
+	float __exp2f_finite(float x) { return exp2f(x); }
+	float __acosf_finite(float x) { return acosf(x); }
+	float __asinf_finite(float x) { return asinf(x); }
+	float __atan2f_finite(float x, float y) { return atan2f(x, y); }
+	float __log2f_finite(float x) { return log2f(x); }
+	float __log10f_finite(float x) { return log10f(x); }
+	float __logf_finite(float x) { return logf(x); }
+	float __powf_finite(float x, float y) { return powf(x, y); }
+}
+
+#endif
