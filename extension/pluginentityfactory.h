97abd4486124eca2316825742e2c2cc2737b61b5
MyGamepedia <103366204+MyGamepedia@users.noreply.github.com>
2026-07-17 17:11:33 +0300

    sensitive classname compare fix
    black mesa doesn't always use lowercase only classnames, so newlights and gonarch doesn't appear, newlight_point will appear but newLight_Point won't (the second case used by maps). this fixes the issue.

--- extension/pluginentityfactory.h
+++ extension/pluginentityfactory.h
@@ -27,6 +27,16 @@ enum PluginEntityFactoryBaseClass_t
 	FACTORYBASECLASS_MAX
 };
 
+struct CaseInsensitiveCompare {  
+    bool operator()(const std::string& a, const std::string& b) const {  
+#ifdef __linux__  
+        return strcasecmp(a.c_str(), b.c_str()) < 0;  
+#else  
+        return _stricmp(a.c_str(), b.c_str()) < 0;  
+#endif  
+    }  
+};
+
 class PluginFactoryEntityRecord_t
 {
 public:
@@ -120,8 +130,8 @@ private:
 	size_t m_BaseClassSizes[ FACTORYBASECLASS_MAX ];
 	CUtlVector< CPluginEntityFactory* > m_Factories;
 	std::map<cell_t, std::unique_ptr<PluginFactoryEntityRecord_t>> m_Records;
-	std::map<std::string, IEntityFactory*> m_gameFactories;
-	std::map<std::string, CPluginEntityFactory*> m_pluginFactories;
+	std::map<std::string, IEntityFactory*, CaseInsensitiveCompare> m_gameFactories; 
+	std::map<std::string, CPluginEntityFactory*, CaseInsensitiveCompare> m_pluginFactories;
 	std::vector<int> m_hookIds;
 };
 
